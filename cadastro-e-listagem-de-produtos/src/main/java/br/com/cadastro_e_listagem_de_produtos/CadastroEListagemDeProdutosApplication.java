0ef5baa63c2b82f5c34587998b7b517367391f14
Giovanni Marques <giovanni.marques030@gmail.com>
2024-10-01 11:03:48 -0300

    chore: initialize project

---
+++ cadastro-e-listagem-de-produtos/src/main/java/br/com/cadastro_e_listagem_de_produtos/CadastroEListagemDeProdutosApplication.java
@@ -0,0 +1,13 @@
+package br.com.cadastro_e_listagem_de_produtos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroEListagemDeProdutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroEListagemDeProdutosApplication.class, args);
+	}
+
+}
---
+++ cadastro-e-listagem-de-produtos/src/test/java/br/com/cadastro_e_listagem_de_produtos/CadastroEListagemDeProdutosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cadastro_e_listagem_de_produtos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroEListagemDeProdutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
