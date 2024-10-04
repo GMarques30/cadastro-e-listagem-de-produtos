af18c4e1fccd012589122f57208374cbfef4e63f
Giovanni Marques <giovanni.marques030@gmail.com>
2024-10-04 08:28:06 -0300

    fix: fix endpoint get controller product

--- src/main/java/br/com/cadastro_e_listagem_de_produtos/infra/http/ProductController.java
+++ src/main/java/br/com/cadastro_e_listagem_de_produtos/infra/http/ProductController.java
@@ -22,7 +22,7 @@ public class ProductController {
     this.listAllProducts = listAllProducts;
   }
 
-  @PostMapping
+  @PostMapping("/create")
   public ResponseEntity<Void> create(@RequestBody CreateProductInput input) {
     try {
       this.createProduct.execute(input);
